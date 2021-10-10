bebbb34c5733d4ba27fbc61897a21817372eb583
Lara <Lara.unimail@gmail.com>
2021-10-10 20:48:24 +0300

    [ADD:] Comment

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -64,6 +64,7 @@ public class SpeedRacer : MonoBehaviour
         return 2021 - pAge;
     }
 
+    // Checks characteristics
     string CheckCharacteristics()
     {
         if (isCarTypeSedan == true)
